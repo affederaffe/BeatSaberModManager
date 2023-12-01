b2221d926319db73d67f73f9d0c3afc45015d28b
affederaffe <68356204+affederaffe@users.noreply.github.com>
2023-12-01 19:49:50 +0100

    Use Dialogs instead of windows

---
+++ BeatSaberModManager/Views/Dialogs/ExceptionDialog.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia.Controls;
+
+
+namespace BeatSaberModManager.Views.Dialogs
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public partial class ExceptionDialog : UserControl
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public ExceptionDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BeatSaberModManager/Views/Dialogs/PickInstallDirDialog.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia.Controls;
+
+
+namespace BeatSaberModManager.Views.Dialogs
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public partial class PickInstallDirDialog : UserControl
+    {
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public PickInstallDirDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
